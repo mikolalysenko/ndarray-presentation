9083243ad75f70f68f29ff4f5c654d8bb26a6fad
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-24 11:44:36 -0500

    adding files

--- index.js
+++ index.js
@@ -2,20 +2,27 @@
 
 var detach = require("remove-element")
 var glIframe = document.createElement("iframe")
+glIframe.setAttribute("src", "gl/index.html")
 
 Reveal.addEventListener("slidechanged", function(event) {
   var prevContainer = event.previousSlide.querySelector(".glcontainer")
   if(prevContainer) {
     glIframe.contentWindow.postMessage(JSON.stringify({
       "event": "hide",
-      "class": event.previousSlide.attr("class")
-    }, "*")
-    detach(demoIframe)
+      "class": Array.prototype.join.call(event.previousSlide.classList)
+    }), "*")
+    detach(glIframe)
   }
   var nextContainer = event.currentSlide.querySelector(".glcontainer")
-  if()
-
+  if(nextContainer) {
+    glIframe.width = nextContainer.clientWidth
+    glIframe.height = nextContainer.clientHeight
+    nextContainer.appendChild(glIframe)
+    glIframe.contentWindow.postMessage(JSON.stringify({
+      "event": "show",
+      "class": Array.prototype.join.call(event.currentSlide.classList)
+    }), "*")
+  }
 })
-
 
 Reveal.initialize({})
